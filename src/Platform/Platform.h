5a0196a6dc2ed75f42b942f01be88b084219c01c
GitMoDu <batatas@gmail.com>
2025-07-15 12:41:30 +0100

    Added explicit platform compatibility, untested platforms are unsupported.

--- src/Platform/Platform.h
+++ src/Platform/Platform.h
@@ -3,7 +3,10 @@
 
 #include <stdint.h>
 
-#if defined(ARDUINO_ARCH_RP2040) || defined(PICO_RP2350)
+#if defined(ARDUINO_ARCH_AVR) || defined(ARDUINO_AVR_MEGA2560)
+#elif defined(ARDUINO_ARCH_STM32_F1) || defined(ARDUINO_ARCH_STM32_F4)
+#elif defined(ARDUINO_ARCH_STM32)
+#elif defined(ARDUINO_ARCH_RP2040) || defined(PICO_RP2350)
 #define HARMONIC_PLATFORM_OS
 #elif defined(ARDUINO_ARCH_NRF52)
 #define HARMONIC_PLATFORM_OS
@@ -11,6 +14,8 @@
 #define HARMONIC_PLATFORM_OS
 #elif defined(WINDOWS)
 #define HARMONIC_PLATFORM_OS
+#else
+#error Harmonic::Platform not supported
 #endif
 
 namespace Harmonic
