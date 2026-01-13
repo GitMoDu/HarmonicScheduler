60bf7788ff589bfe557031610b4b65da5b5f3725
GitModu <batatas@gmail.com>
2026-01-13 19:45:48 +0000

    Fixed typo.

--- src/HarmonicSchedulerCompatibility.h
+++ src/HarmonicSchedulerCompatibility.h
@@ -1,5 +1,5 @@
 #ifndef _HARMONIC_SCHEDULER_COMPATIBILITY_INCLUDE_h
-#define _HARMONIC_SCHEDULER_COMPATIBILITY_INCLUDE_hs
+#define _HARMONIC_SCHEDULER_COMPATIBILITY_INCLUDE_h
 
 #include "HarmonicScheduler.h"
 #include "Task/CompatibilityTask.h"
