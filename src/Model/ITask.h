9ac01ad26a6957c53d64ca95a083beba765f5789
GitModu <batatas@gmail.com>
2026-01-13 19:46:45 +0000

    Updated ITask model.

--- src/Model/ITask.h
+++ src/Model/ITask.h
@@ -11,9 +11,13 @@ namespace Harmonic
 	/// Classes implementing ITask must provide a Run() method containing the task's run callback.
 	/// Tasks are expected to be non-blocking and complete execution quickly (typically under 1 ms).
 	/// </summary>
-	class ITask
+	struct ITask
 	{
-	public:
+		/// <summary>
+		/// Virtual destructor for the ITask interface.
+		/// </summary>
+		virtual ~ITask() = default;
+
 		/// <summary>
 		/// Task execution callback.
 		/// Override this method to implement the task's behavior.
@@ -21,6 +25,10 @@ namespace Harmonic
 		/// </summary>
 		virtual void Run() = 0;
 
+		/// <summary>
+		/// Called when a task's ID is updated. Implementations must store their new task ID, if any scheduling changes are needed.
+		/// </summary>
+		/// <param name="taskId">The new task ID value.</param>
 		virtual void OnTaskIdUpdated(const task_id_t taskId) = 0;
 	};
 }
